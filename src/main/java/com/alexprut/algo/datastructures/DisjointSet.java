642f1e55f6a8ac673f1ee2e17eb2ff5e87bbb0cc
Alex Prut <1648497+alexprut@users.noreply.github.com>
2018-10-07 13:56:30 +0100

    Basic Data Structure: Disjoint Set

--- src/main/java/com/alexprut/algo/datastructures/DisjointSet.java
+++ src/main/java/com/alexprut/algo/datastructures/DisjointSet.java
@@ -1,5 +1,70 @@
 package com.alexprut.algo.datastructures;
 
+import java.util.HashMap;
+
 public class DisjointSet {
 
+  private HashMap<Integer, Element> sets = new HashMap<>();
+
+  public Element makeSet(int value) {
+    sets.put(value, new Element(value));
+    return sets.get(value);
+  }
+
+  public void union(Element a, Element b) {
+    if (a.getRank() > b.getRank()) {
+      b.setParent(a);
+    } else {
+      a.setParent(b);
+      if (a.getRank() == b.getRank()) {
+        b.setRank(b.getRank() + 1);
+      }
+    }
+  }
+
+  public Element findSet(Element s) {
+    if (s.parent().getValue() == s.getValue()) {
+      return s;
+    }
+
+    s.setParent(findSet(s.parent()));
+    return s.parent();
+  }
+
+  public Element getElement(int value) {
+    return sets.get(value);
+  }
+
+  class Element {
+
+    private Element parent;
+    private int rank;
+    private int value;
+
+    Element(int value) {
+      this.value = value;
+      this.rank = 0;
+      this.parent = this;
+    }
+
+    public int getRank() {
+      return this.rank;
+    }
+
+    public void setRank(int rank) {
+      this.rank = rank;
+    }
+
+    public int getValue() {
+      return this.value;
+    }
+
+    public Element parent() {
+      return this.parent;
+    }
+
+    public void setParent(Element parent) {
+      this.parent = parent;
+    }
+  }
 }
--- src/test/java/com/alexprut/algo/datastructures/DisjointSetTest.java
+++ src/test/java/com/alexprut/algo/datastructures/DisjointSetTest.java
@@ -1,8 +1,22 @@
 package com.alexprut.algo.datastructures;
 
+import com.alexprut.algo.datastructures.DisjointSet.Element;
+import org.junit.Assert;
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class DisjointSetTest {
+
+  @Test
+  public void shouldUnionSets() {
+    DisjointSet sets = new DisjointSet();
+    Element a = sets.makeSet(1);
+    Element b = sets.makeSet(2);
+    sets.union(a, b);
+    Assert.assertEquals(a.parent().getValue(), b.getValue());
+    Assert.assertEquals(b.parent().getValue(), b.getValue());
+    Assert.assertEquals(b.getRank(), 1);
+  }
 
 }
