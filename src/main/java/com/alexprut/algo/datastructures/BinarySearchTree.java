dc130dcb0789aa99c87ba7d42f15c1da4f7d5535
Alex Prut <1648497+alexprut@users.noreply.github.com>
2018-10-22 23:21:53 +0100

    feat: BinarySearchTree search, contains methods

--- src/main/java/com/alexprut/algo/datastructures/BinarySearchTree.java
+++ src/main/java/com/alexprut/algo/datastructures/BinarySearchTree.java
@@ -9,6 +9,10 @@ public class BinarySearchTree {
     size = 0;
   }
 
+  /**
+   * Time complexity:
+   * O(logn) if the tree is balanced, O(n) in the worst case
+   */
   public void insert(int value) {
     BinaryNode node = new BinaryNode(value);
 
@@ -20,24 +24,113 @@ public class BinarySearchTree {
     size++;
   }
 
+  /**
+   * Time complexity:
+   * O(logn) if the tree is balanced, O(n) in the worst case
+   */
+  public BinaryNode search(int value) {
+    return search(root, value);
+  }
+
+  /**
+   * Time complexity:
+   * O(logn) if the tree is balanced, O(n) in the worst case
+   */
+  public void remove(BinaryNode node) {
+    // TODO
+  }
+
+  // TODO successor
+  // TODO predecessor
+  // TODO inOrderVisit
+  // TODO preOrderVisit
+  // TODO postOrderVisit
+
+  /**
+   * Time complexity:
+   * O(logn) if the tree is balanced, O(n) in the worst case
+   * TODO interactive
+   */
+  private BinaryNode search(BinaryNode node, int value) {
+    if (node == null || node.value == value) {
+      return node;
+    }
+    if (node.value < value) {
+      return search(node.right, value);
+    }
+    return search(node.left, value);
+  }
+
+  /**
+   * Time complexity:
+   * O(logn) if the tree is balanced, O(n) in the worst case
+   */
+  public boolean contains(int value) {
+    return search(root, value) != null;
+  }
+
   public int size() {
     return this.size;
   }
 
+  /**
+   * Time complexity:
+   * O(logn) if the tree is balanced, O(n) in the worst case
+   */
   private void insert(BinaryNode parent, BinaryNode node) {
-    if (parent.value() <= node.value()) {
-      if (parent.left == null) {
-        parent.left = node;
-      } else {
-        insert(parent.left, node);
-      }
-    } else {
+    if (parent.value() < node.value()) {
       if (parent.right == null) {
         parent.right = node;
       } else {
         insert(parent.right, node);
       }
+    } else {
+      if (parent.left == null) {
+        parent.left = node;
+      } else {
+        insert(parent.left, node);
+      }
     }
+  }
+
+  /**
+   * Time complexity:
+   * O(logn) if the tree is balanced, O(n) in the worst case
+   */
+  public BinaryNode minimum() {
+    return minimum(root);
+  }
+
+  /**
+   * Time complexity:
+   * O(logn) if the tree is balanced, O(n) in the worst case
+   */
+  public BinaryNode maximum() {
+    return maximum(root);
+  }
+
+  /**
+   * Time complexity:
+   * O(logn) if the tree is balanced, O(n) in the worst case
+   * TODO interactive
+   */
+  private BinaryNode minimum(BinaryNode node) {
+    if (node == null || node.left == null) {
+      return node;
+    }
+    return minimum(node.left);
+  }
+
+  /**
+   * Time complexity:
+   * O(logn) if the tree is balanced, O(n) in the worst case
+   * TODO interactive
+   */
+  private BinaryNode maximum(BinaryNode node) {
+    if (node == null || node.right == null) {
+      return node;
+    }
+    return maximum(node.right);
   }
 
   public static class BinaryNode {
--- src/test/java/com/alexprut/algo/datastructures/BinarySearchTreeTest.java
+++ src/test/java/com/alexprut/algo/datastructures/BinarySearchTreeTest.java
@@ -6,17 +6,26 @@ import org.junit.Test;
 public class BinarySearchTreeTest {
 
   @Test
-  public void shouldInsert() {
+  public void shouldInsertAndRetrieve() {
     BinarySearchTree bst = new BinarySearchTree();
     Assert.assertEquals(0, bst.size());
 
     bst.insert(1);
     Assert.assertEquals(1, bst.size());
+    Assert.assertTrue(bst.contains(1));
+    Assert.assertEquals(1, bst.search(1).value());
 
     bst.insert(1);
     Assert.assertEquals(2, bst.size());
+    Assert.assertTrue(bst.contains(1));
+    Assert.assertEquals(1, bst.search(1).value());
 
     bst.insert(2);
     Assert.assertEquals(3, bst.size());
+    Assert.assertTrue(bst.contains(2));
+    Assert.assertEquals(2, bst.search(2).value());
+
+    Assert.assertEquals(2, bst.maximum().value());
+    Assert.assertEquals(1, bst.minimum().value());
   }
 }
