80185e0a77c527c73939cd5d16f7e236ca8be164
Alex Prut <1648497+alexprut@users.noreply.github.com>
2020-02-26 10:42:00 +0100

    fix: formating

--- src/main/java/com/alexprut/algo/datastructures/MerkleTree.java
+++ src/main/java/com/alexprut/algo/datastructures/MerkleTree.java
@@ -6,91 +6,91 @@ import java.security.MessageDigest;
 import java.util.ArrayList;
 
 public class MerkleTree {
-	private String[] hashNodes;
+  private String[] hashNodes;
 
-	public MerkleTree(final ArrayList<String> elements) {
-		if (elements.size() % 2 != 0) {
-			elements.add(elements.get(elements.size() - 1));
-		}
-		hashNodes = new String[elements.size() * 2 - 1];
-		for (int i = 1; i <= elements.size(); i++) {
-			hashNodes[hashNodes.length - i] = hash(elements.get(elements.size() - i));
-		}
-		build(0);
-	}
+  public MerkleTree(final ArrayList<String> elements) {
+    if (elements.size() % 2 != 0) {
+      elements.add(elements.get(elements.size() - 1));
+    }
+    hashNodes = new String[elements.size() * 2 - 1];
+    for (int i = 1; i <= elements.size(); i++) {
+      hashNodes[hashNodes.length - i] = hash(elements.get(elements.size() - i));
+    }
+    build(0);
+  }
 
-	/** Time complexity: Θ(n) */
-	protected void build(int i) {
-		if (hashNodes[i] == null) {
-			build(left(i));
-			build(right(i));
-			hashNodes[i] = hash(hashNodes[left(i)] + hashNodes[right(i)]);
-		}
-	}
+  /** Time complexity: Θ(n) */
+  protected void build(int i) {
+    if (hashNodes[i] == null) {
+      build(left(i));
+      build(right(i));
+      hashNodes[i] = hash(hashNodes[left(i)] + hashNodes[right(i)]);
+    }
+  }
 
-	/** Time complexity: Θ(1) */
-	protected static int parent(int i) {
-		return (i + 1) / 2 - 1;
-	}
+  /** Time complexity: Θ(1) */
+  protected static int parent(int i) {
+    return (i + 1) / 2 - 1;
+  }
 
-	/** Time complexity: Θ(1) */
-	protected static int left(int i) {
-		return 2 * i + 1;
-	}
+  /** Time complexity: Θ(1) */
+  protected static int left(int i) {
+    return 2 * i + 1;
+  }
 
-	/** Time complexity: Θ(1) */
-	protected static int right(int i) {
-		return 2 * i + 2;
-	}
+  /** Time complexity: Θ(1) */
+  protected static int right(int i) {
+    return 2 * i + 2;
+  }
 
-	/** Time complexity: Θ(1) */
-	public String root() {
-		return hashNodes[0];
-	}
+  /** Time complexity: Θ(1) */
+  public String root() {
+    return hashNodes[0];
+  }
 
-	/** Time complexity: Θ(logn) */
-	public ArrayList<String> getProofPath(int elementIndex) {
-		ArrayList<String> proofPath = new ArrayList<>();
-		int elements = (hashNodes.length + 1) / 2;
-		int tmpNodeIndex = hashNodes.length - elements + elementIndex;
-		int tmpParentIndex = parent(tmpNodeIndex);
-		while (tmpNodeIndex != 0) {
-			if (left(tmpParentIndex) != tmpNodeIndex) {
-				proofPath.add(hashNodes[left(tmpParentIndex)]);
-			} else {
-				proofPath.add(hashNodes[right(tmpParentIndex)]);
-			}
-			tmpNodeIndex = tmpParentIndex;
-			tmpParentIndex = parent(tmpParentIndex);
-		}
-		return proofPath;
-	}
+  /** Time complexity: Θ(logn) */
+  public ArrayList<String> getProofPath(int elementIndex) {
+    ArrayList<String> proofPath = new ArrayList<>();
+    int elements = (hashNodes.length + 1) / 2;
+    int tmpNodeIndex = hashNodes.length - elements + elementIndex;
+    int tmpParentIndex = parent(tmpNodeIndex);
+    while (tmpNodeIndex != 0) {
+      if (left(tmpParentIndex) != tmpNodeIndex) {
+        proofPath.add(hashNodes[left(tmpParentIndex)]);
+      } else {
+        proofPath.add(hashNodes[right(tmpParentIndex)]);
+      }
+      tmpNodeIndex = tmpParentIndex;
+      tmpParentIndex = parent(tmpParentIndex);
+    }
+    return proofPath;
+  }
 
-	/** Time complexity: Θ(logn) */
-	public static boolean verify(String element, String rootHash, final ArrayList<String> proofPath) {
-		String tmpHash = hash(element);
-		for (String s : proofPath) {
-			tmpHash = hash(tmpHash + s);
-		}
-		return tmpHash.equals(rootHash);
-	}
+  /** Time complexity: Θ(logn) */
+  public static boolean verify(String element, String rootHash, final ArrayList<String> proofPath) {
+    String tmpHash = hash(element);
+    for (String s : proofPath) {
+      tmpHash = hash(tmpHash + s);
+    }
+    return tmpHash.equals(rootHash);
+  }
 
-	public static String hash(String content) {
-		try {
-			MessageDigest digest = MessageDigest.getInstance("SHA-256");
-			String tmpSha256hex = toHexString(digest.digest(content.getBytes(StandardCharsets.UTF_8)));
-			return toHexString(digest.digest(tmpSha256hex.getBytes(StandardCharsets.UTF_8)));
-		} catch (Exception e) {
-			return "";
-		}
-	}
+  public static String hash(String content) {
+    try {
+      MessageDigest digest = MessageDigest.getInstance("SHA-256");
+      String tmpSha256hex = toHexString(digest.digest(content.getBytes(StandardCharsets.UTF_8)));
+      return toHexString(digest.digest(tmpSha256hex.getBytes(StandardCharsets.UTF_8)));
+    } catch (Exception e) {
+      return "";
+    }
+  }
 
-	private static String toHexString(final byte[] hash) {
-		BigInteger number = new BigInteger(1, hash);
-		StringBuilder hexString = new StringBuilder(number.toString(16));
-		while (hexString.length() < 32) {
-			hexString.insert(0, '0');
-		}
-		return hexString.toString();
-	}
+  private static String toHexString(final byte[] hash) {
+    BigInteger number = new BigInteger(1, hash);
+    StringBuilder hexString = new StringBuilder(number.toString(16));
+    while (hexString.length() < 32) {
+      hexString.insert(0, '0');
+    }
+    return hexString.toString();
+  }
 }
