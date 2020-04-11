1602722ebfad00b7d6a154b064423ba07ceacb93
Alex Prut <1648497+alexprut@users.noreply.github.com>
2020-04-11 14:09:29 +0200

    test(FibonacciHeap): decrease key and delete methods

--- src/test/java/com/alexprut/algo/datastructures/FibonacciHeapTest.java
+++ src/test/java/com/alexprut/algo/datastructures/FibonacciHeapTest.java
@@ -1,5 +1,6 @@
 package com.alexprut.algo.datastructures;
 
+import com.alexprut.algo.datastructures.FibonacciHeap.Node;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -19,5 +20,22 @@ public class FibonacciHeapTest {
     heap.insert(1);
     Assert.assertEquals(new Integer(1), heap.extractMin().key);
     Assert.assertEquals(0, heap.size);
+  }
+
+  @Test
+  public void shouldDecreaseKey() {
+    FibonacciHeap<Integer> heap = new FibonacciHeap<>();
+    Node<Integer> x = heap.insert(2);
+    heap.decreaseKey(x, 1);
+    Assert.assertEquals(new Integer(1), x.key);
+  }
+
+  @Test
+  public void shouldDeleteNode() {
+    FibonacciHeap<Integer> heap = new FibonacciHeap<>();
+    Node<Integer> x = heap.insert(2);
+    heap.delete(x);
+    Assert.assertEquals(0, heap.size);
+    Assert.assertEquals(null, heap.min);
   }
 }
