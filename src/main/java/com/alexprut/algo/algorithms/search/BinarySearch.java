e5b193dfce5a63c9e70ef05984d53c52d09b9485
Alex Prut <1648497+alexprut@users.noreply.github.com>
2020-04-26 11:33:13 +0200

    feat: add binarySearchFirstLowest and binarySearchFirstHighest methods

--- src/main/java/com/alexprut/algo/algorithms/search/BinarySearch.java
+++ src/main/java/com/alexprut/algo/algorithms/search/BinarySearch.java
@@ -22,4 +22,46 @@ public class BinarySearch {
 
     return -1;
   }
+
+  /** Time complexity: O(logn) */
+  public static int binarySearchFirstLowest(int[] a, int v) {
+    int start = 0;
+    int end = a.length - 1;
+
+    while (start <= end) {
+      int middle = start + (end - start) / 2;
+      if (a[middle] == v) {
+        return middle;
+      }
+
+      if (v < a[middle]) {
+        end = middle - 1;
+      } else {
+        start = middle + 1;
+      }
+    }
+
+    return end;
+  }
+
+  /** Time complexity: O(logn) */
+  public static int binarySearchFirstHighest(int[] a, int v) {
+    int start = 0;
+    int end = a.length - 1;
+
+    while (start <= end) {
+      int middle = start + (end - start) / 2;
+      if (a[middle] == v) {
+        return middle;
+      }
+
+      if (v < a[middle]) {
+        end = middle - 1;
+      } else {
+        start = middle + 1;
+      }
+    }
+
+    return start;
+  }
 }
