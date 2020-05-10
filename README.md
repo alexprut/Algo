23474c787f5715eee3f8a692fbb7876d83ba0312
Alex Prut <1648497+alexprut@users.noreply.github.com>
2020-05-10 15:16:18 +0200

    feat(RabinKarp): search pattern in string algo

--- README.md
+++ README.md
@@ -39,7 +39,7 @@ Algorithms
 |[Dijkstra](https://github.com/alexprut/Algo/blob/master/src/main/java/com/alexprut/algo/algorithms/graph/Dijkstra.java)|O(\|E\| + \|V\|log\|V\|)|-|-|-|
 |[Maximum SubArray](https://github.com/alexprut/Algo/blob/master/src/main/java/com/alexprut/algo/algorithms/MaximumSubArray.java)|Θ(n)|Θ(n)|Θ(n)|Θ(1)|
 |Knuth-Morris-Pratt|-|-|-|-|
-|Rabin-Karp|-|-|-|-|
+|[Rabin-Karp](https://github.com/alexprut/Algo/blob/master/src/main/java/com/alexprut/algo/algorithms/RabinKarp.java)|Θ((n - m + 1)m)|Θ(n)|Θ(n)|-|
 |[Greatest common divisor (GCD)](https://github.com/alexprut/Algo/blob/master/src/main/java/com/alexprut/algo/algorithms/math/Math.java#L13)|-|-|-|-|
 |[Binary Search](https://github.com/alexprut/Algo/blob/master/src/main/java/com/alexprut/algo/algorithms/search/BinarySearch.java)|O(nlogn)|O(nlogn)|O(1)|Θ(1)|
 |[Breadth First Search (BFS)](https://github.com/alexprut/Algo/blob/master/src/main/java/com/alexprut/algo/algorithms/search/BreadthFirstSearch.java#L43)|O(\|E\|+\|V\|)|O(\|E\|+\|V\|)|-|-|
--- src/main/java/com/alexprut/algo/algorithms/RabinKarp.java
+++ src/main/java/com/alexprut/algo/algorithms/RabinKarp.java
@@ -1,3 +1,54 @@
 package com.alexprut.algo.algorithms;
 
-public class RabinKarp {}
+public class RabinKarp {
+
+  /**
+   * A string matching algorithm proposed by Rabin and Karp. The algorithm makes use of elementary
+   * number-theoretic notions such as the equivalence of two numbers modulo a third number.
+   *
+   * <p>Time complexity: Θ((n - m + 1)m)
+   *
+   * <p>Space complexity: Θ(n) total with Θ(1) auxiliary
+   *
+   * @param text the text to search in
+   * @param pattern the pattern to search within the text
+   * @param d the size of the alphabet
+   * @return the index of the first occurrence of the match, -1 if there is no occurrence
+   */
+  public static int rabinKarp(String text, String pattern, int d, int q) {
+    int n = text.length();
+    int m = pattern.length();
+    // Value of the high-order digit position of an m-digit window
+    int h = ((int) Math.pow(d, m - 1)) % q;
+    int p = 0;
+    int t = 0;
+    for (int i = 0; i < m; i++) {
+      // Using Horner's rule
+      p = (d * p + pattern.charAt(i)) % q;
+      t = (d * t + text.charAt(i)) % q;
+    }
+    for (int s = 0; s <= n - m; s++) {
+      if (p == t) {
+        // spurious hit
+        boolean isMatch = true;
+        for (int i = 0; i < m; i++) {
+          if (pattern.charAt(i) != text.charAt(s + i)) {
+            isMatch = false;
+          }
+          if (isMatch) {
+            return s;
+          }
+        }
+      }
+      if (s < n - m) {
+        int prev = text.charAt(s);
+        int succ = text.charAt(s + m);
+        t = (d * (t - prev * h) + succ) % q;
+        if (t < 0) {
+          t = q + t;
+        }
+      }
+    }
+    return -1;
+  }
+}
