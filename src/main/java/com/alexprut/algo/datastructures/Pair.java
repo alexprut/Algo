58f44e8117153f67cc0295fe75394d73fda6427b
Alex Prut <1648497+alexprut@users.noreply.github.com>
2020-05-15 17:09:59 +0200

    docs(Javadoc): wiki for Pair

--- src/main/java/com/alexprut/algo/datastructures/Pair.java
+++ src/main/java/com/alexprut/algo/datastructures/Pair.java
@@ -1,5 +1,11 @@
 package com.alexprut.algo.datastructures;
 
+/**
+ * Utility data structure for gluing pairs of other data structures.
+ *
+ * @param <F> type of first element
+ * @param <S> type of second element
+ */
 public class Pair<F, S> {
 
   private F first;
@@ -10,10 +16,20 @@ public class Pair<F, S> {
     this.second = second;
   }
 
+  /**
+   * Get the first element
+   *
+   * @return first element
+   */
   public F first() {
     return this.first;
   }
 
+  /**
+   * Get the second element
+   *
+   * @return second element
+   */
   public S second() {
     return this.second;
   }
