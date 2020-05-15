bd96827ec83dd1ed76ca2810f2b3f821313499d0
Alex Prut <1648497+alexprut@users.noreply.github.com>
2020-05-15 17:11:36 +0200

    docs(Javadoc): wiki for SegmentTree

--- src/main/java/com/alexprut/algo/datastructures/SegmentTree.java
+++ src/main/java/com/alexprut/algo/datastructures/SegmentTree.java
@@ -2,6 +2,9 @@ package com.alexprut.algo.datastructures;
 
 /**
  * Stores intervals, and optimized for "which of these intervals contains a given point" queries.
+ *
+ * @see <a
+ *     href="https://en.wikipedia.org/wiki/Segment_tree">https://en.wikipedia.org/wiki/Segment_tree</a>
  */
 abstract class SegmentTree {
 
@@ -15,7 +18,18 @@ abstract class SegmentTree {
     build(0, elements.length - 1, 0);
   }
 
-  /** Time complexity: O(logn) */
+  /**
+   * Time complexity: O(logn)
+   *
+   * <p>Space complexity: TODO
+   *
+   * @param start
+   * @param end
+   * @param startInterval
+   * @param endInterval
+   * @param current
+   * @return
+   */
   private int search(int start, int end, int startInterval, int endInterval, int current) {
     if (startInterval <= start && endInterval >= end) {
       return tree[current];
@@ -31,13 +45,30 @@ abstract class SegmentTree {
         search(mid + 1, end, startInterval, endInterval, right(current)));
   }
 
-  /** Time complexity: O(1) */
+  /**
+   * Time complexity: O(1)
+   *
+   * @return
+   */
   protected abstract int searchUtilDefault();
 
-  /** Time complexity: O(1) */
+  /**
+   * Time complexity: O(1)
+   *
+   * @param left
+   * @param right
+   * @return
+   */
   protected abstract int calculate(int left, int right);
 
-  /** Time complexity: O(logn) */
+  /**
+   * Time complexity: O(logn)
+   *
+   * @param start
+   * @param end
+   * @param i
+   * @param current
+   */
   private void update(int start, int end, int i, int current) {
     if (i < start || i > end) {
       return;
@@ -55,19 +86,37 @@ abstract class SegmentTree {
     tree[current] = calculate(tree[left(current)], tree[right(current)]);
   }
 
-  /** Time complexity: O(logn) */
+  /**
+   * Time complexity: O(logn)
+   *
+   * @param i
+   * @param newValue
+   */
   public void update(int i, int newValue) {
     elements[i] = newValue;
 
     update(0, elements.length - 1, i, 0);
   }
 
-  /** Time complexity: O(logn) */
+  /**
+   * Time complexity: O(logn)
+   *
+   * @param startInterval
+   * @param endInterval
+   * @return
+   */
   public int search(int startInterval, int endInterval) {
     return search(0, elements.length - 1, startInterval, endInterval, 0);
   }
 
-  /** Time complexity: O(n) */
+  /**
+   * Time complexity: O(n)
+   *
+   * @param startInterval
+   * @param endInterval
+   * @param current
+   * @return
+   */
   private int build(int startInterval, int endInterval, int current) {
     if (startInterval == endInterval) {
       tree[current] = elements[startInterval];
