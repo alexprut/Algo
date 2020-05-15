8513b47c827983db390d84aad1169807768b815e
Alex Prut <1648497+alexprut@users.noreply.github.com>
2020-05-15 17:12:03 +0200

    docs(Javadoc): wiki for BinarySearch

--- src/main/java/com/alexprut/algo/algorithms/search/BinarySearch.java
+++ src/main/java/com/alexprut/algo/algorithms/search/BinarySearch.java
@@ -1,8 +1,18 @@
 package com.alexprut.algo.algorithms.search;
 
+/**
+ * @see <a
+ *     href="https://en.wikipedia.org/wiki/Binary_search_algorithm">https://en.wikipedia.org/wiki/Binary_search_algorithm</a>
+ */
 public class BinarySearch {
 
-  /** Time complexity: O(logn) */
+  /**
+   * Time complexity: O(logn)
+   *
+   * @param a
+   * @param v
+   * @return
+   */
   public static int binarySearch(int[] a, int v) {
     int start = 0;
     int end = a.length - 1;
@@ -23,7 +33,13 @@ public class BinarySearch {
     return -1;
   }
 
-  /** Time complexity: O(logn) */
+  /**
+   * Time complexity: O(logn)
+   *
+   * @param a
+   * @param v
+   * @return
+   */
   public static int binarySearchFirstLowest(int[] a, int v) {
     int start = 0;
     int end = a.length - 1;
@@ -44,7 +60,13 @@ public class BinarySearch {
     return end;
   }
 
-  /** Time complexity: O(logn) */
+  /**
+   * Time complexity: O(logn)
+   *
+   * @param a
+   * @param v
+   * @return
+   */
   public static int binarySearchFirstHighest(int[] a, int v) {
     int start = 0;
     int end = a.length - 1;
