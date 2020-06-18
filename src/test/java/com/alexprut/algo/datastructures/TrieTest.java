a2ddb8f4be56d065e94396698b2f4aef4597e418
Alex Prut <1648497+alexprut@users.noreply.github.com>
2020-06-18 12:21:05 +0200

    feat(Trie): refactor and add remove methods

--- src/test/java/com/alexprut/algo/datastructures/TrieTest.java
+++ src/test/java/com/alexprut/algo/datastructures/TrieTest.java
@@ -6,16 +6,105 @@ import org.junit.Test;
 public class TrieTest {
 
   @Test
-  public void shouldInsertAndFind() {
+  public void shouldInsertFindAndRemove() {
+    /*
+     *                ""
+     *               / | \
+     *              d  s  a
+     *             /   |   \
+     *            a    t    l
+     *           /     |     \
+     *          t      r      g
+     *         / \     |      |
+     *       e*   a*   u      o*
+     *                 |      |
+     *                 c      r
+     *                 |      |
+     *                 t*     i
+     *                 |      |
+     *                 u      t
+     *                 |      |
+     *                 r      h
+     *                 |      |
+     *                 e*     m
+     */
     Trie trie = new Trie();
+    Assert.assertEquals(0, trie.size());
     trie.insert("data");
+    trie.insert("date");
     trie.insert("structure");
-    trie.insert("algorithms");
+    trie.insert("struct");
+    trie.insert("algorithm");
+    trie.insert("algo");
 
-    Assert.assertTrue(trie.contains("algo"));
-    Assert.assertTrue(trie.contains("algorithm"));
-    Assert.assertTrue(trie.containsWord("algorithms"));
-    Assert.assertTrue(trie.containsWord("structure"));
-    Assert.assertTrue(trie.containsWord("data"));
+    Assert.assertEquals(6, trie.size());
+    Assert.assertFalse(trie.search(""));
+    Assert.assertFalse(trie.searchPrefix(""));
+    Assert.assertTrue(trie.search("algo"));
+    Assert.assertTrue(trie.search("algorithm"));
+    Assert.assertTrue(trie.searchPrefix("algorithm"));
+    Assert.assertTrue(trie.searchPrefix("algorith"));
+    Assert.assertFalse(trie.searchPrefix("algorithms"));
+    Assert.assertTrue(trie.searchPrefix("structure"));
+    Assert.assertTrue(trie.searchPrefix("data"));
+    Assert.assertEquals(2, trie.sizePrefix("alg"));
+    Assert.assertEquals(2, trie.sizePrefix("algo"));
+
+    Assert.assertFalse(trie.remove(""));
+    Assert.assertFalse(trie.remove("alg"));
+    Assert.assertFalse(trie.remove("algos"));
+    Assert.assertEquals(6, trie.size());
+    /*
+     *                 ""
+     *               / | \
+     *              d  s  a
+     *             /   |   \
+     *            a    t    l
+     *           /     |     \
+     *          t      r      g
+     *         / \     |      |
+     *       e*   a*   u      o
+     *                 |      |
+     *                 c      r
+     *                 |      |
+     *                 t*     i
+     *                 |      |
+     *                 u      t
+     *                 |      |
+     *                 r      h
+     *                 |      |
+     *                 e*     m
+     */
+    Assert.assertTrue(trie.remove("algo"));
+    Assert.assertEquals(5, trie.size());
+
+    Assert.assertFalse(trie.removePrefix(""));
+    /*
+     *                 ""
+     *                 | \
+     *                 s  a
+     *                 |   \
+     *                 t    l
+     *                 |     \
+     *                 r      g
+     *                 |      |
+     *                 u      o*
+     *                 |      |
+     *                 c      r
+     *                 |      |
+     *                 t*     i
+     *                 |      |
+     *                 u      t
+     *                 |      |
+     *                 r      h
+     *                 |      |
+     *                 e*     m
+     */
+    Assert.assertTrue(trie.removePrefix("dat"));
+    Assert.assertEquals(3, trie.size());
+
+    Assert.assertTrue(trie.removePrefix("s"));
+    Assert.assertTrue(trie.removePrefix("a"));
+    Assert.assertEquals(0, trie.size());
   }
 }
