3814193749c7e5f2d5856823fcf6ef8d3f88e6e3
Alex Prut <1648497+alexprut@users.noreply.github.com>
2020-06-26 14:41:55 +0200

    docs(Javadoc): add queue example

--- src/main/java/com/alexprut/algo/datastructures/Queue.java
+++ src/main/java/com/alexprut/algo/datastructures/Queue.java
@@ -1,9 +1,18 @@
 package com.alexprut.algo.datastructures;
 
 /**
- * FIFO
+ * A Queue is a collection of entities that are maintained in a sequence and can be modified by the
+ * addition of entities at one end of the sequence and the removal of entities from the other end of
+ * the sequence. In a queue, the element deleted is always the one that has been in the set for the
+ * longest time: the queue implements a first-in, first-out, or FIFO, policy.
  *
- * <p>TODO
+ * <p>Example:
+ *
+ * <pre>
+ * ————                   ————————                 ————
+ *  24  -> enqueue(42) ->  42, 24   -> dequeue() -> 42
+ * ————                   ————————                 ————
+ * </pre>
  *
  * @see <a
  *     href="https://en.wikipedia.org/wiki/Queue_(abstract_data_type)">https://en.wikipedia.org/wiki/Queue_(abstract_data_type)</a>
@@ -17,37 +26,53 @@ public class Queue<T> {
   }
 
   /**
-   * Time complexity: Θ(1)
+   * Check if the queue is empty.
    *
-   * @return
+   * <p>Time complexity: Θ(1)
+   *
+   * <p>Space complexity: Θ(1)
+   *
+   * @return true if empty
    */
   public boolean empty() {
     return list.empty();
   }
 
   /**
-   * Time complexity: Θ(1)
+   * Add the new element in front of the queue.
    *
-   * @param value
+   * <p>Time complexity: Θ(1)
+   *
+   * <p>Space complexity: Θ(1)
+   *
+   * @param value the value of the element to insert
    */
   public void enqueue(T value) {
     list.insertFront(value);
   }
 
   /**
-   * Time complexity: Θ(1)
+   * Removes the last element from the queue.
    *
-   * @return
-   * @throws Exception
+   * <p>Time complexity: Θ(1)
+   *
+   * <p>Space complexity: Θ(1)
+   *
+   * @return the removed element
+   * @throws Exception if the queue is empty
    */
   public T dequeue() throws Exception {
     return list.removeBack();
   }
 
   /**
-   * Time complexity: Θ(1)
+   * The number of elements within the queue.
    *
-   * @return
+   * <p>Time complexity: Θ(1)
+   *
+   * <p>Space complexity: Θ(1)
+   *
+   * @return the size of the queue
    */
   public int size() {
     return list.size();
