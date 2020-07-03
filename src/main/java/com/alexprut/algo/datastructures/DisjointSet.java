2351b8289562a2e2b0bebd4a32fbb1eb35192f82
Alex Prut <1648497+alexprut@users.noreply.github.com>
2020-07-03 10:30:58 +0200

    feat(Javadoc): add Disjoint set docs

--- src/main/java/com/alexprut/algo/datastructures/DisjointSet.java
+++ src/main/java/com/alexprut/algo/datastructures/DisjointSet.java
@@ -3,19 +3,30 @@ package com.alexprut.algo.datastructures;
 import java.util.HashMap;
 
 /**
+ * A disjoint-set data structure (also called a union–find data structure or merge–find set) is a
+ * data structure that tracks a set of elements partitioned into a number of disjoint
+ * (non-overlapping) subsets. It provides near-constant-time operations. To improve the running
+ * time, the following heuristics are used: union by rank and path compression.
+ *
+ * <p>TODO add example
+ *
  * @see <a
  *     href="https://en.wikipedia.org/wiki/Disjoint-set_data_structure">https://en.wikipedia.org/wiki/Disjoint-set_data_structure</a>
  * @param <T>
  */
 public class DisjointSet<T> {
 
-  private HashMap<T, Element<T>> sets = new HashMap<>();
+  private final HashMap<T, Element<T>> sets = new HashMap<>();
 
   /**
-   * TODO
+   * Creates a new set.
    *
-   * @param value
-   * @return
+   * <p>Time complexity: O(1)
+   *
+   * <p>Space complexity: O(1)
+   *
+   * @param value the element value
+   * @return the new set containing one element
    */
   public Element<T> makeSet(T value) {
     sets.put(value, new Element<>(value));
@@ -23,10 +34,14 @@ public class DisjointSet<T> {
   }
 
   /**
-   * TODO
+   * Union of two sets.
    *
-   * @param a
-   * @param b
+   * <p>Time complexity: O(1)
+   *
+   * <p>Space complexity: O(1)
+   *
+   * @param a first set
+   * @param b second set
    */
   public void union(Element<T> a, Element<T> b) {
     Element<T> parentA = findSet(a);
@@ -47,10 +62,14 @@ public class DisjointSet<T> {
   }
 
   /**
-   * TODO
+   * Given an element finds the set to with it belongs.
    *
-   * @param s
-   * @return
+   * <p>Time complexity: O(1)
+   *
+   * <p>Space complexity: O(1)
+   *
+   * @param s the element to find
+   * @return the set to witch the element belongs
    */
   public Element<T> findSet(Element<T> s) {
     if (s.parent().getValue() == s.getValue()) {
@@ -62,30 +81,38 @@ public class DisjointSet<T> {
   }
 
   /**
-   * TODO
+   * Given an element finds the set to with it belongs.
    *
-   * @param value
-   * @return
+   * <p>Time complexity: O(1)
+   *
+   * <p>Space complexity: O(1)
+   *
+   * @param value the element value to find
+   * @return the set to witch the element belongs
    */
   public Element<T> findSet(T value) {
     return findSet(getElement(value));
   }
 
   /**
-   * TODO
+   * Given the value find the element.
    *
-   * @param value
-   * @return
+   * <p>Time complexity: O(1)
+   *
+   * <p>Space complexity: O(1)
+   *
+   * @param value the element value to find
+   * @return the element to witch the value belongs
    */
   public Element<T> getElement(T value) {
     return sets.get(value);
   }
 
-  class Element<T> {
+  static class Element<T> {
 
     private Element<T> parent;
     private int rank;
-    private T value;
+    private final T value;
 
     Element(T value) {
       this.value = value;
@@ -97,7 +124,7 @@ public class DisjointSet<T> {
       return this.rank;
     }
 
-    public void setRank(int rank) {
+    protected void setRank(int rank) {
       this.rank = rank;
     }
 
@@ -109,7 +136,7 @@ public class DisjointSet<T> {
       return this.parent;
     }
 
-    public void setParent(Element<T> parent) {
+    protected void setParent(Element<T> parent) {
       this.parent = parent;
     }
   }
