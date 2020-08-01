040e1a442ffc493b67e1582c75f03e176a4cf19c
Alex Prut <1648497+alexprut@users.noreply.github.com>
2020-08-01 13:07:55 +0200

    feat(Javadoc): add bubble sort docs

--- src/main/java/com/alexprut/algo/algorithms/sorting/BubbleSort.java
+++ src/main/java/com/alexprut/algo/algorithms/sorting/BubbleSort.java
@@ -5,14 +5,21 @@ import com.alexprut.algo.Utils;
 public class BubbleSort {
 
   /**
-   * TODO
+   * Bubble sort is a simple sorting algorithm that repeatedly steps through the list, compares
+   * adjacent elements and swaps them if they are in the wrong order. The pass through the list is
+   * repeated until the list is sorted.
    *
-   * <p>Time complexity: O(n^2) in the worst case, O(n) in the best case, when the array is sorted
+   * <p>Example: given the elements [5, 2, 1, 9, 3, 3, 0] the sorted elements would be [0, 1, 2, 3,
+   * 3, 5, 9]
+   *
+   * <p>Time complexity: O(n^2) in the worst case, O(n) in the best case when the array is sorted
    *
    * <p>Space complexity: O(n) total with O(1) auxiliary
    *
-   * @param arr
-   * @return
+   * @see <a
+   *     href="https://en.wikipedia.org/wiki/Bubble_sort">https://en.wikipedia.org/wiki/Bubble_sort</a>
+   * @param arr the elements to sort
+   * @return the sorted elements
    */
   public static int[] bubbleSort(int[] arr) {
     boolean swapped = true;
