9a5c6c7c64bfd998349cf5ad658fbf1201ae7125
Alex Prut <1648497+alexprut@users.noreply.github.com>
2020-08-01 09:36:15 +0200

    feat(Javadoc): add MaximumSubarray docs

--- src/main/java/com/alexprut/algo/algorithms/MaximumSubarray.java
+++ src/main/java/com/alexprut/algo/algorithms/MaximumSubarray.java
@@ -7,14 +7,17 @@ public class MaximumSubarray {
    * Finds the contiguous sub-array within a one-dimensional array, a[1...n], of numbers which has
    * the largest sum.
    *
+   * <p>Example: given the elements [−2, 1, −3, 4, −1, 2, 1, −5, 4], the contiguous subarray with
+   * the largest sum is [4, −1, 2, 1], with sum 6.
+   *
    * <p>Time complexity: Θ(n)
    *
    * <p>Space complexity: Θ(n) total with Θ(1) auxiliary
    *
    * @see <a
    *     href="https://en.wikipedia.org/wiki/Maximum_subarray_problem">https://en.wikipedia.org/wiki/Maximum_subarray_problem</a>
-   * @param elements
-   * @return
+   * @param elements the elements
+   * @return the largest contiguous sub-array sum
    */
   public static int maximumSubarray(int[] elements) {
     if (elements.length == 0) {
