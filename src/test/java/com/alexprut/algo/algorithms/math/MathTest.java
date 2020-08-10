b5c6f99e66b3433574ae8aab100e0751069c04a3
Alex Prut <1648497+alexprut@users.noreply.github.com>
2020-08-10 12:46:17 +0200

    feat(Javadoc): add documentation for Math algos

--- src/test/java/com/alexprut/algo/algorithms/math/MathTest.java
+++ src/test/java/com/alexprut/algo/algorithms/math/MathTest.java
@@ -14,8 +14,9 @@ public class MathTest {
     Assert.assertEquals(Math.gcd(0, 1), 1);
     Assert.assertEquals(Math.gcd(1, 0), 1);
     Assert.assertEquals(Math.gcd(10, 2), 2);
-    Assert.assertEquals(Math.gcd(10, 10), 10);
     Assert.assertEquals(Math.gcd(2, 10), 2);
+    Assert.assertEquals(Math.gcd(6, 10), 2);
+    Assert.assertEquals(Math.gcd(10, 10), 10);
   }
 
   @Test
