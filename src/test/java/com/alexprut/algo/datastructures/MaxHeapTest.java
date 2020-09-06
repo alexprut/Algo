27865c1fb647852ceac0b61b392829e3f6d99dba
Alex Prut <1648497+alexprut@users.noreply.github.com>
2020-09-06 14:43:52 +0200

    test: add test MaxHeap

--- src/test/java/com/alexprut/algo/datastructures/MaxHeapTest.java
+++ src/test/java/com/alexprut/algo/datastructures/MaxHeapTest.java
@@ -8,35 +8,59 @@ public class MaxHeapTest {
   @Test
   public void shouldSort() {
     int[] toSort = new int[] {5, 2, 1, 9, 0, 33, 3, 3, 0};
-    int[] expected = new int[] {0, 0, 1, 2, 3, 3, 5, 9, 33};
+    int[] expected = new int[] {33, 9, 5, 3, 3, 2, 1, 0, 0};
 
-    BinaryHeap heap = new MaxHeap(toSort);
+    MaxHeap heap = new MaxHeap(toSort);
     heap.heapsort();
     Assert.assertArrayEquals(expected, heap.elements());
   }
 
   @Test
   public void shouldSortInverseSorted() {
-    int[] toSort = new int[] {33, 9, 5, 3, 3, 2, 1, 0, 0};
-    int[] expected = new int[] {0, 0, 1, 2, 3, 3, 5, 9, 33};
+    int[] toSort = new int[] {0, 0, 1, 2, 3, 3, 5, 9, 33};
+    int[] expected = new int[] {33, 9, 5, 3, 3, 2, 1, 0, 0};
 
-    BinaryHeap heap = new MaxHeap(toSort);
+    MaxHeap heap = new MaxHeap(toSort);
     heap.heapsort();
     Assert.assertArrayEquals(expected, heap.elements());
   }
 
   @Test
   public void shouldSortSorted() {
-    int[] expected = new int[] {0, 0, 1, 2, 3, 3, 5, 9, 33};
+    int[] expected = new int[] {33, 9, 5, 3, 3, 2, 1, 0, 0};
 
-    BinaryHeap heap = new MaxHeap(expected);
+    MaxHeap heap = new MaxHeap(expected);
     heap.heapsort();
     Assert.assertArrayEquals(expected, heap.elements());
   }
 
   @Test
+  public void shouldInsertExtractGetMaxIncreaseKey() {
+    int[] expected = new int[] {33, 0, 0};
+
+    MaxHeap heap = new MaxHeap();
+    heap.insert(33);
+    heap.insert(0);
+    heap.insert(0);
+    heap.heapsort();
+
+    Assert.assertArrayEquals(expected, heap.elements());
+    Assert.assertEquals(33, heap.max());
+    Assert.assertEquals(3, heap.size());
+
+    try {
+      Assert.assertEquals(33, heap.extractMax());
+      heap.increaseKey(1, 2);
+    } catch (Exception e) {
+    }
+
+    Assert.assertEquals(2, heap.size());
+    Assert.assertEquals(2, heap.max());
+  }
+
+  @Test
   public void shouldHandleEdgeCases() {
-    BinaryHeap heap = new MaxHeap(new int[] {});
+    MaxHeap heap = new MaxHeap(new int[] {});
     heap.heapsort();
     Assert.assertArrayEquals(new int[] {}, heap.elements());
 
