6d8c200a88eb7f9b4a1fc8114aad2fb3f9ac291a
Alex Prut <1648497+alexprut@users.noreply.github.com>
2020-09-07 18:08:39 +0200

    test: BST successor and delete methods

--- src/test/java/com/alexprut/algo/datastructures/BinarySearchTreeTest.java
+++ src/test/java/com/alexprut/algo/datastructures/BinarySearchTreeTest.java
@@ -137,4 +137,38 @@ public class BinarySearchTreeTest {
 
     Assert.assertEquals(expected, bst.postOrderVisit());
   }
+
+  @Test
+  public void shouldGetSuccessorAndDelete() {
+    /*
+             4
+          /    \
+         2      6
+        / \    / \
+       1   3  5   7
+    */
+    BinarySearchTree<Integer> bst = new BinarySearchTree<>();
+    bst.insert(4);
+    bst.insert(2);
+    bst.insert(1);
+    bst.insert(3);
+    bst.insert(6);
+    bst.insert(5);
+    bst.insert(7);
+
+    Assert.assertEquals(5, (int) bst.successor(bst.search(4)).value());
+    Assert.assertEquals(2, (int) bst.successor(bst.search(1)).value());
+    Assert.assertEquals(7, (int) bst.successor(bst.search(6)).value());
+    Assert.assertEquals(null, bst.successor(bst.search(7)));
+
+    bst.delete(bst.search(6));
+    bst.delete(bst.search(3));
+    bst.delete(bst.search(2));
+    bst.delete(bst.search(4));
+    bst.delete(bst.search(1));
+    bst.delete(bst.search(5));
+    bst.delete(bst.search(7));
+
+    Assert.assertEquals(0, bst.size());
+  }
 }
