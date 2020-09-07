5d3e278865dcf4839af18ecb30167cf6f6557f29
Alex Prut <1648497+alexprut@users.noreply.github.com>
2020-09-08 00:21:27 +0200

    test: intervaltree successor and delete methods

--- src/test/java/com/alexprut/algo/datastructures/IntervalTreeTest.java
+++ src/test/java/com/alexprut/algo/datastructures/IntervalTreeTest.java
@@ -43,4 +43,53 @@ public class IntervalTreeTest {
     Assert.assertFalse(IntervalTree.doOverlap(new IntervalNode(0, 3), new IntervalNode(4, 5)));
     Assert.assertFalse(IntervalTree.doOverlap(new IntervalNode(4, 5), new IntervalNode(0, 3)));
   }
+
+  @Test
+  public void shouldDoMultipleOperations() {
+    /*
+                        [16,21],30,B
+                /                         \
+          [8,9],23,R                    [25,30],30,R
+         /         \                   /            \
+       [5,8],10,B  [15,23],23,B    [17,19],20,B    [26,26],26,B
+      /         \                            \
+    [0,3],3,R  [6,10],10,R               [19,20],20,R
+    */
+    IntervalTree tree = new IntervalTree();
+    tree.insert(16, 21);
+    tree.insert(8, 9);
+    tree.insert(25, 30);
+    tree.insert(5, 8);
+    tree.insert(15, 23);
+    tree.insert(17, 19);
+    tree.insert(26, 26);
+    tree.insert(0, 3);
+    tree.insert(6, 10);
+    tree.insert(19, 20);
+
+    Assert.assertEquals(0, tree.minimum().value());
+    Assert.assertEquals(26, tree.maximum().value());
+    Assert.assertNotNull(tree.find(4, 9));
+    Assert.assertNotNull(tree.find(9, 9));
+    Assert.assertNotNull(tree.find(10, 14));
+    Assert.assertNull(tree.find(31, 32));
+    Assert.assertNull(tree.find(-2, -1));
+    Assert.assertEquals(
+        17, tree.successor(tree.search(tree.root(), new IntervalNode(16, 21))).value());
+    Assert.assertEquals(
+        25, tree.successor(tree.search(tree.root(), new IntervalNode(19, 20))).value());
+
+    tree.delete(16, 21);
+    tree.delete(5, 8);
+    tree.delete(17, 19);
+    tree.delete(25, 30);
+    tree.delete(0, 3);
+    tree.delete(8, 9);
+    tree.delete(15, 23);
+    tree.delete(19, 20);
+    tree.delete(26, 26);
+    tree.delete(6, 10);
+
+    Assert.assertEquals(0, tree.size());
+  }
 }
