520531d018c8e47520d2b3397560b3a4c69481c0
Alex Prut <1648497+alexprut@users.noreply.github.com>
2020-09-12 00:54:50 +0200

    test: AVL Tree min, max, removal

--- src/test/java/com/alexprut/algo/datastructures/AVLTreeTest.java
+++ src/test/java/com/alexprut/algo/datastructures/AVLTreeTest.java
@@ -16,6 +16,7 @@ public class AVLTreeTest {
 
     Assert.assertNotNull(tree.root());
     Assert.assertNull(tree.root().parent());
+    Assert.assertEquals(new Integer(0), tree.minimum().value());
     Assert.assertEquals(new Integer(99), tree.maximum().value());
   }
 
@@ -104,5 +105,52 @@ public class AVLTreeTest {
     Assert.assertEquals(null, u.right());
     Assert.assertEquals(null, z.left());
     Assert.assertEquals(null, z.right());
+  }
+
+  @Test
+  public void shouldDoMultipleOperations() {
+    /*
+           34
+         /    \
+       13      68
+      /  \    /  \
+     8   27  41  71
+    */
+
+    AVLTree<Integer> tree = new AVLTree<>();
+    tree.insert(71);
+    tree.insert(34);
+    tree.insert(8);
+    tree.insert(41);
+    tree.insert(68);
+    tree.insert(13);
+    tree.insert(27);
+
+    Assert.assertEquals(7, tree.size());
+    Assert.assertTrue(tree.search(71));
+    Assert.assertTrue(tree.search(34));
+    Assert.assertTrue(tree.search(8));
+    Assert.assertTrue(tree.search(41));
+    Assert.assertTrue(tree.search(68));
+    Assert.assertTrue(tree.search(13));
+    Assert.assertTrue(tree.search(27));
+
+    Assert.assertNull(tree.successor(tree.search(tree.root, 71)));
+    Assert.assertEquals(41, (int) tree.successor(tree.search(tree.root, 34)).value());
+    Assert.assertEquals(13, (int) tree.successor(tree.search(tree.root, 8)).value());
+    Assert.assertEquals(68, (int) tree.successor(tree.search(tree.root, 41)).value());
+    Assert.assertEquals(71, (int) tree.successor(tree.search(tree.root, 68)).value());
+    Assert.assertEquals(27, (int) tree.successor(tree.search(tree.root, 13)).value());
+    Assert.assertEquals(34, (int) tree.successor(tree.search(tree.root, 27)).value());
+
+    tree.delete(34);
+    tree.delete(13);
+    tree.delete(41);
+    tree.delete(71);
+    tree.delete(27);
+    tree.delete(8);
+    tree.delete(68);
+
+    Assert.assertEquals(0, tree.size());
   }
 }
